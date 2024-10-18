bc675b5513ff2ff7e080b8ec2f782fc183b18508
refarefa1 <refaelavramov@gmail.com>
2024-10-18 16:12:57 +0300

    add configs for deploy

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -5,17 +5,11 @@ export const HomePage = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        // navigate('/calendar')
+        navigate('/calendar')
     }, [])
 
     return (
-        <section style={{ height: '100%' , display:'flex',}}>
-
-
-            <div style={{ width: '210px', background: 'red', left: '200px', position:'relative',height: '100%'}}></div>
-            <div style={{ display: 'flex', position:'relative', background: 'blue', flexGrow: 1 }}>
-                
-            </div>
+        <section>
         </section>
     );
 };
